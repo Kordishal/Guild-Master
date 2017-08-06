5efcf1abaf8403fd40f4e9e281bf46f79800092d
SoulLink <kordishal@gmail.com>
2017-08-07 00:07:56 +0100

    Added Skills Adventurers can now have skills with which they complete missions.

---
+++ Guild Master/Assets/Scripts/AdventurerSkills.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurerSkills : MonoBehaviour {
+
+    public static List<Skill> AllSkills;
+
+	// Use this for initialization
+	void Start () {
+
+        AllSkills = new List<Skill>();
+
+        for (int i = 0; i < (int)SkillNames.LAST_ENTRY; i++)
+        {
+            AllSkills.Add(new Skill((SkillNames)System.Enum.GetValues(typeof(SkillNames)).GetValue(i), 0));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public class Skill
+    {
+        public SkillNames Name;
+        public int Level;
+        public int throwDiceVs(int Difficulty)
+        {
+            return UnityEngine.Random.Range(0, 20) + Level - Difficulty;
+        }
+
+        public Skill(SkillNames name, int level)
+        {
+            Name = name;
+            Level = level;
+        }
+
+
+    }
+
+    public enum SkillNames
+    {
+        Walking,
+        Investigate,
+        Tracking,
+        Perception,
+
+
+
+        LAST_ENTRY
+    }
+}
