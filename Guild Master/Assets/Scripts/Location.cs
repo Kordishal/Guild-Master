2821fc56d2c593feaa851e4b6bcdf2748d860ab7
SoulLink <kordishal@gmail.com>
2017-08-07 00:10:57 +0100

    Added locations. Represents any kind of place in the world.

---
+++ Guild Master/Assets/Scripts/Location.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour {
+
+    public string Name;
+    public string Description;
+
+    public List<Adventurer> Adventurers;
+
+    // Use this for initialization
+    void Start () {
+        Adventurers = new List<Adventurer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+
+    public override bool Equals(object other)
+    {
+        return Name == ((Location)other).Name;
+    }
+}
