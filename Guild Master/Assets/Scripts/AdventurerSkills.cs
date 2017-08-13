8c7ade7a7d5107539e6b99a18b09084cdd77acb7
SoulLink <kordishal@gmail.com>
2017-08-13 14:17:50 +0100

    Skill level factors now properly increase skill levels instead of decreasing it.

--- Guild Master/Assets/Scripts/AdventurerSkills.cs
+++ Guild Master/Assets/Scripts/AdventurerSkills.cs
@@ -13,11 +13,11 @@ public class AdventurerSkills : MonoBehaviour {
 
         // Movement Skills:
 
-        AllSkills[(int)SkillNames.Walking] = new Skill("Walking", SkillType.Movement, 0, 0, 1, 100, 5, 0.01f);
-        AllSkills[(int)SkillNames.Running] = new Skill("Running", SkillType.Movement, 0, 0, 1, 300, 0.2, 0.005f);
-        AllSkills[(int)SkillNames.Riding] = new Skill("Horse Riding", SkillType.Movement, 0, 0, 1, 500, 1, 0.01f);
-        AllSkills[(int)SkillNames.Teleportation] = new Skill("Teleporting", SkillType.Movement, 0, 0, 1, 500, 1, 0.1f);
-        AllSkills[(int)SkillNames.PortalCreation] = new Skill("Portal Creation", SkillType.Movement, 0, 0, 1, 1000, 1, 0.1f);
+        AllSkills[(int)SkillNames.Walking] = new Skill("Walking", SkillType.Movement, 0, 0, 1, 100, 5, 1.01f);
+        AllSkills[(int)SkillNames.Running] = new Skill("Running", SkillType.Movement, 0, 0, 1, 300, 0.2, 1.005f);
+        AllSkills[(int)SkillNames.Riding] = new Skill("Horse Riding", SkillType.Movement, 0, 0, 1, 500, 1, 1.01f);
+        AllSkills[(int)SkillNames.Teleportation] = new Skill("Teleporting", SkillType.Movement, 0, 0, 1, 500, 1, 1.1f);
+        AllSkills[(int)SkillNames.PortalCreation] = new Skill("Portal Creation", SkillType.Movement, 0, 0, 1, 1000, 1, 1.1f);
 
         // Perception Skills:
 
